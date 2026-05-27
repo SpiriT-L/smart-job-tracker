617ca82e452da799ce9e5f42401714fc2f27ed2d
LeonID <lmatskevich79@gmail.com>
2026-05-28 00:25:54 +0300

    feat: add auth database models

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,14 @@
-// prisma.config.ts
-import { config } from 'dotenv';
+import 'dotenv/config';
+
 import { defineConfig, env } from 'prisma/config';
 
-// Укажите правильный путь до вашего .env файла
-config({ path: '.env' });
-
 export default defineConfig({
+  schema: 'prisma/schema.prisma',
+
+  migrations: {
+    path: 'prisma/migrations',
+  },
+
   datasource: {
     url: env('DATABASE_URL'),
   },
