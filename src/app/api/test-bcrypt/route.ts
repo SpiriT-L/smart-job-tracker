84055d21d2bdeb0e81a653c8e91522a40aff7995
LeonID <lmatskevich79@gmail.com>
2026-06-08 22:38:30 +0300

    feat: add password hashing

--- src/app/api/test-bcrypt/route.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt';
-
-export async function GET() {
-  const hash = await bcrypt.hash('123456', 10);
-  const isMatch = await bcrypt.compare('123456', hash);
-
-  return Response.json({ hash, isMatch });
-}
--- src/app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from '@/shared/api/prisma';
-
-export async function GET() {
-  const users = await prisma.user.findMany();
-
-  return Response.json(users);
-}
