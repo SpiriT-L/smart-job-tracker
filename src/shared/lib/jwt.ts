26b4ef0c5ac7305165925b831814a3b5f7fa47cf
LeonID <lmatskevich79@gmail.com>
2026-07-08 00:05:33 +0300

    feat: add JWT signing functionality and jose dependency

---
+++ src/shared/lib/jwt.ts
@@ -0,0 +1,19 @@
+// import { SignJWT } from 'jose';
+
+// type JwtPayload = {
+//   sub: string;
+//   email: string;
+//   role: Role;
+// };
+
+// function getJwtSecret() {
+//   const secret = process.env.JWT_SECRET;
+//   if (!secret) {
+//     throw new Error('JWT_SECRET is not defined');
+//   }
+//   return secret;
+// }
+
+// export async function signJWT(payload: JwtPayload) {
+//   const secret = TextEncoder().encode(getJwtSecret());
+// }
